6b72e776a3acb3747c05f0b4386c8b284d9b6a12
Rafiul Islam Hredoy <64517450+Kaka-Co@users.noreply.github.com>
2023-08-19 17:57:31 +0600

    project initiation

---
+++ src/components/Home.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Todos from "./Todos";
+const dummyTodos = [
+  { id: 1, title: "todo title 1", desc: "todo1 description is here ....." },
+];
+
+const Home = () => {
+  return (
+    <div>
+      <Todos todos={dummyTodos} />
+    </div>
+  );
+};
+
+export default Home;
